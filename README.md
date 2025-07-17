fdb3b98bfafa3ecc6f3e62427c4d0ec2e1e268cf
maidem <admin@maidem.de>
2025-07-17 03:36:39 +0200

    add deploy.php

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Get going quickly with TYPO3 CMS.
 
 ## Prerequisites
 
-* PHP 8.2
-* [Composer](https://getcomposer.org/download/)
+- PHP 8.4
+- [Composer](https://getcomposer.org/download/)
 
 ## Quickstart
 
-* `composer create-project typo3/cms-base-distribution project-name ^13`
-* `cd project-name`
+- `composer create-project typo3/cms-base-distribution project-name ^13`
+- `cd project-name`
 
 Note that this distribution installs most, but not all of the TYPO3 CMS core extensions.
 Depending on your need you might also want to install other TYPO3 extensions from
@@ -50,16 +50,16 @@ While it's advised to use a more sophisticated web server such as
 Apache 2 or Nginx, you can instantly run the project by using PHPs` built-in
 [web server](https://secure.php.net/manual/en/features.commandline.webserver.php).
 
-* `TYPO3_CONTEXT=Development php -S localhost:8000 -t public`
-* open your browser at "http://localhost:8000"
+- `TYPO3_CONTEXT=Development php -S localhost:8000 -t public`
+- open your browser at "http://localhost:8000"
 
 Please be aware that the built-in web server is single threaded and only meant
 to be used for development.
 
-##  Next steps
+## Next steps
 
-* [Getting Started with TYPO3](https://docs.typo3.org/permalink/t3start:start)
-* [Create a Site Package](https://docs.typo3.org/permalink/t3sitepackage:start)
+- [Getting Started with TYPO3](https://docs.typo3.org/permalink/t3start:start)
+- [Create a Site Package](https://docs.typo3.org/permalink/t3sitepackage:start)
 
 ## License
 
---
+++ deploy.php
@@ -0,0 +1,86 @@
+<?php
+namespace Deployer;
+
+require 'recipe/typo3.php';
+
+set('repository', 'git@github.com:maidem/taekwondo-mueller.git');
+
+host('live')
+    ->set('hostname', getenv('DEPLOY_HOST') ?: 'ssh.isenstedt.project.host')
+    ->set('remote_user', getenv('DEPLOY_USER') ?: 'maik.demuth@outlook.com@a-gzfyfw')
+    ->set('deploy_path', getenv('DEPLOY_PATH') ?: '/html/typo3-wuuy9')
+    ->set('branch', getenv('DEPLOY_BRANCH') ?: 'main')
+    ->set('identity_file', '~/.ssh/id_ed25519');
+
+set('web_path', 'public/');
+set('bin/php', '/usr/local/bin/php');
+
+
+/**
+ * Bereinigung von fehlerhaften Releases
+ */
+task('deploy:clean_release', function () {
+    $nextRelease = (int)get('release_name') ?: count(get('releases_list')) + 1;
+    if (test("[ -d {{deploy_path}}/releases/{$nextRelease} ]")) {
+        run("rm -rf {{deploy_path}}/releases/{$nextRelease}");
+    }
+});
+
+/**
+ * Erzwungenes Entsperren bei fehlgeschlagenem Deployment
+ */
+task('deploy:force_unlock', function () {
+    if (test("[ -f {{deploy_path}}/.dep/deploy.lock ]")) {
+        run("rm -f {{deploy_path}}/.dep/deploy.lock");
+    }
+});
+
+/**
+ * Löscht alte Releases und hält maximal 5 Versionen
+ */
+task('cleanup', function () {
+    $releases = get('releases_list');
+    $keep = 5;
+    if (count($releases) <= $keep) {
+        return;
+    }
+    $releasesToDelete = array_slice($releases, $keep);
+    foreach ($releasesToDelete as $release) {
+        run("rm -rf {{deploy_path}}/releases/{$release}");
+    }
+});
+
+/**
+ * TYPO3 Cache leeren
+ */
+task('typo3:cache:flush', function () {
+    run('{{bin/php}} {{release_path}}/vendor/bin/typo3 cache:flush');
+});
+
+/**
+ * Deployment-Workflow
+ */
+task('deploy', [
+    'deploy:prepare',
+    'deploy:force_unlock',
+    'deploy:lock',
+    'deploy:clean_release',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:vendors',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
+
+/**
+ * Führe Cache Flush nach erfolgreichem Deployment aus
+ */
+after('deploy', 'typo3:cache:flush');
+
+/**
+ * Falls ein Deployment fehlschlägt, entferne den Lock
+ */
+after('deploy:failed', 'deploy:unlock');
