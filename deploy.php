24afb7f7b5e5dfa486a1c280492968039415a1da
maidem <connect@maidem.de>
2025-07-20 14:55:14 +0200

    fix deploy

--- deploy.php
+++ deploy.php
@@ -10,10 +10,13 @@ set('shared_dirs', []);
 set('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
+
 host('production')
     ->set('hostname', getenv('DEPLOY_HOST'))
     ->set('remote_user', getenv('DEPLOY_USER'))
     ->set('deploy_path', getenv('DEPLOY_PATH'))
     ->set('identity_file', '~/.ssh/taekwondo-deployer');
+
+set('git_ssh_command', 'ssh -i ~/.ssh/taekwondo-deployer -o StrictHostKeyChecking=accept-new');
 
 after('deploy:failed', 'deploy:unlock');
