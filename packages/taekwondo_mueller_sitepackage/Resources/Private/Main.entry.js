51cd342fd4ee2e28c3938afb6d0ad5901c021a2d
maidem <connect@maidem.de>
2025-10-28 18:17:26 +0100

    New run Build

--- packages/taekwondo_mueller_sitepackage/Resources/Private/Main.entry.js
+++ packages/taekwondo_mueller_sitepackage/Resources/Private/Main.entry.js
@@ -1,3 +1,4 @@
-import "./Main.entry.scss";
+import "bootstrap/scss/bootstrap.scss";
+import "../Public/Scss/layout.scss";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "../Public/JavaScript/main.js";
--- public/_assets/vite/assets/Main.entry-CSvb7yYp.js
+++ public/_assets/vite/assets/Main.entry-CSvb7yYp.js
@@ -1,4 +1,4 @@
-/* empty css                   */var Et={exports:{}};/*!
+var Et={exports:{}};/*!
   * Bootstrap v5.3.8 (https://getbootstrap.com/)
   * Copyright 2011-2025 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
