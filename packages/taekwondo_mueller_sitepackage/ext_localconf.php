8339fbeea75e7764a9925db48d96065469484339
maidem <connect@maidem.de>
2025-11-03 19:05:48 +0100

    Add Testnavigation

--- packages/taekwondo_mueller_sitepackage/ext_localconf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('TYPO3') or die('Access denied.');
-
-// Add default RTE configuration
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['taekwondo_mueller_sitepackage'] = 'EXT:taekwondo_mueller_sitepackage/Configuration/RTE/Default.yaml';
